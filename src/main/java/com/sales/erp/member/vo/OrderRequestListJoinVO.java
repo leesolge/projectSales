b9d8f97de1169eb6df30bcf0323ec43e165db145
Anseok_Kosta <leeanseok@gmail.com>
2016-11-18 09:45:37 +0900

    ..

--- src/main/java/com/sales/erp/member/vo/OrderRequestListJoinVO.java
+++ src/main/java/com/sales/erp/member/vo/OrderRequestListJoinVO.java
@@ -2,98 +2,44 @@ package com.sales.erp.member.vo;
 
 import java.util.Date;
 
-public class OrderRequestListJoinVO {
-
-	private int cnt, oamount, checks, deleted;
-	private String onum, empno, procode, ocomment, team, title;
-	private Date regdate;
-
+public class OrderRequestListJoinVO extends MemberVO {
+	private int cnt;
+	private String onum, empno, title, procode;
+	private Date regdate;	
 	public int getCnt() {
 		return cnt;
 	}
-
 	public void setCnt(int cnt) {
 		this.cnt = cnt;
 	}
-
-	public int getOamount() {
-		return oamount;
-	}
-
-	public void setOamount(int oamount) {
-		this.oamount = oamount;
-	}
-
-	public int getChecks() {
-		return checks;
-	}
-
-	public void setChecks(int checks) {
-		this.checks = checks;
-	}
-
-	public int getDeleted() {
-		return deleted;
-	}
-
-	public void setDeleted(int deleted) {
-		this.deleted = deleted;
-	}
-
 	public String getOnum() {
 		return onum;
 	}
-
 	public void setOnum(String onum) {
 		this.onum = onum;
 	}
-
 	public String getEmpno() {
 		return empno;
 	}
-
 	public void setEmpno(String empno) {
 		this.empno = empno;
 	}
-
-	public String getProcode() {
-		return procode;
-	}
-
-	public void setProcode(String procode) {
-		this.procode = procode;
-	}
-
-	public String getOcomment() {
-		return ocomment;
-	}
-
-	public void setOcomment(String ocomment) {
-		this.ocomment = ocomment;
-	}
-
-	public String getTeam() {
-		return team;
-	}
-
-	public void setTeam(String team) {
-		this.team = team;
-	}
-
-	public Date getRegdate() {
-		return regdate;
-	}
-
-	public void setRegdate(Date regdate) {
-		this.regdate = regdate;
-	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
+	public String getProcode() {
+		return procode;
+	}
+	public void setProcode(String procode) {
+		this.procode = procode;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}	
 }
